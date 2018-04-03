cc13c5f724ae0d084ab8c8c956b00abecba97dfc
stevensilalahi <32074612+stevensilalahi@users.noreply.github.com>
2018-04-03 13:43:19 -0700

    added string morse converter program

---
+++ stringMorseConverter/stringMorseConverter/main.cpp
@@ -0,0 +1,112 @@
+//Steven Silalahi
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//functions declared
+
+void convertToMorse (string s);
+void morseCondition(char c);
+//******** MAIN *********************************
+
+int main()
+{
+    string word;
+    
+    //String input
+    cout<<"Enter a word: ";
+    cin >> word;
+    
+    //calls function for morse conversion.
+    cout<<"Morse: ";
+    convertToMorse(word);
+    cout<<""<<endl;
+    
+    return 0;
+}
+
+//***********************************************
+
+
+//this function goes through the string letter by letter
+//and convert them to morse code and put them together
+void convertToMorse(string s)
+{
+    char theChar;
+    //goes through the entire string
+    for (int i=0; i<s.length(); i++)
+    {
+        theChar= s.at(i);
+        //cout<< theChar<<endl;
+        
+        //at index i, the char gets
+        //convertyed to morse
+        morseCondition(theChar);
+    }
+}
+
+//this function gives the morse code to every letter
+void morseCondition (char c)
+{
+    //using switch statement for every condition in each letter
+    switch(c)
+    {
+        case 'a': cout<<".-";
+            break;
+        case 'b': cout<<"-...";
+            break;
+        case 'c': cout<<"-.-.";
+            break;
+        case 'd': cout<<"-..";
+            break;
+        case 'e': cout<<".";
+            break;
+        case 'f': cout<<"..-.";
+            break;
+        case 'g': cout<<"--.";
+            break;
+        case 'h': cout<<"....";
+            break;
+        case 'i': cout<<"..";
+            break;
+        case 'j': cout<<".---";
+            break;
+        case 'k': cout<<"-.-";
+            break;
+        case 'l': cout<<".-..";
+            break;
+        case 'm': cout<<"--";
+            break;
+        case 'n': cout<<"-.";
+            break;
+        case 'o': cout<<"---";
+            break;
+        case 'p': cout<<".--.";
+            break;
+        case 'q': cout<<"--.-";
+            break;
+        case 'r': cout<<".-.";
+            break;
+        case 's': cout<<"...";
+            break;
+        case 't': cout<<"-";
+            break;
+        case 'u': cout<<"..-";
+            break;
+        case 'v': cout<<"...-";
+            break;
+        case 'w': cout<<".--";
+            break;
+        case 'x': cout<<"-..-";
+            break;
+        case 'y': cout<<"-.--";
+            break;
+        case 'z': cout<<"--..";
+            break;
+        case ' ': cout<< " ";
+            break;
+    }
+}
+
+
